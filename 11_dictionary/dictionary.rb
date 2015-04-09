3acad5816ba3c15f9a3abba21b5faf8a0d42816f
Oleh Belkot <o.belkot@gmail.com>
2015-04-09 22:43:22 +0300

    11_dictionary done.

---
+++ 11_dictionary/dictionary.rb
@@ -0,0 +1,31 @@
+class Dictionary
+
+  attr_reader :entries
+
+  def initialize
+    @entries = {}
+  end
+
+  def add(val)
+    val.is_a?(Hash) ? @entries.merge!(val) : @entries[val] = nil
+  end
+
+  def keywords
+    @entries.keys.sort
+  end
+
+  def include?(val)
+    @entries.has_key? val
+  end
+
+  def find(val)
+    @entries.select { |e| e.include? val }
+  end
+
+  def printable
+    result_string = ""
+    keywords.each { |k| result_string += "[#{k}] \"#{@entries[k]}\"\n" }
+    result_string.chomp
+  end
+
+end
